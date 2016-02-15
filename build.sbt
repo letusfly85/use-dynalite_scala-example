e21036e777f1f264340f5b62552f992b4f911e6f
letusfly85 <letusfly95@gmail.com>
2016-02-15 17:17:14 +0900

    add using dynalite sample

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.11.7"
 
 libraryDependencies ++= {
   Seq(
-    "com.amazonaws" % "aws-java-sdk" % "1.10.52"
+    "com.amazonaws" % "aws-java-sdk" % "1.10.52",
+    "commons-configuration" % "commons-configuration" % "1.10"
   )
 }
+
--- src/main/scala/com/jellyfish85/dynalite/UseDynalite.scala
+++ src/main/scala/com/jellyfish85/dynalite/UseDynalite.scala
@@ -1,15 +1,44 @@
 package com.jellyfish85.dynalite
 
+import java.io.InputStream
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import com.amazonaws.services.dynamodbv2.document.DynamoDB
+import com.amazonaws.services.dynamodbv2.model.{ScanResult, ScanRequest}
+
+
+import org.apache.commons.configuration.{PropertiesConfiguration, Configuration}
+
 /**
   * Created by letusfly95 on 2016/02/15.
   */
 object UseDynalite {
 
-
+  val dynamo: DynamoDB = new DynamoDB(new AmazonDynamoDBClient(
+    new ProfileCredentialsProvider())
+  )
 
   def main(args: Array[String]): Unit = {
+    val inputStream: InputStream =
+      getClass.getResourceAsStream("/endpoint.properties")
 
+    val configuration: PropertiesConfiguration =
+      new PropertiesConfiguration()
 
+    configuration.load(inputStream, "UTF8")
+
+    val endpoint = configuration.getString("dynalite.endpoint")
+
+    val client = new AmazonDynamoDBClient(new ProfileCredentialsProvider())
+    client.setEndpoint(endpoint)
+
+    println(client.describeTable("Movies"))
+
+    val scanRequest: ScanRequest = new ScanRequest().withTableName("Movies")
+    val result: ScanResult = client.scan(scanRequest)
+
+    result.getItems.toArray.toList.foreach(println)
 
   }
 
